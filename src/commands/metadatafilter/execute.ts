d3e56b2af1512ae26588e0645a64b99f29a005e4
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2018-05-29 00:44:13 +0200

    v0

---
+++ src/commands/metadatafilter/execute.ts
@@ -0,0 +1,132 @@
+import {Command, flags} from '@oclif/command'
+
+export default class ExecuteFilter extends Command {
+  static description = 'describe the command here'
+
+  static examples = [
+    `$ oclif-example execute -p myPackage.xml
+`,
+    `$ oclif-example execute -p myPackage.xml -output myDeployFolder
+`,
+  ]
+
+  static flags = {
+    // flag with a value (-n, --name=VALUE)
+    packagexml: flags.string({char: 'p', description: 'Package.xml file'}),
+    inputfolder: flags.string({char: 'i', description: 'Input folder (default: current folder)'}),
+    outputfolder: flags.string({char: 'o', description: 'Output folder (default: filteredMetadatas)'})
+  }
+
+  static args = []
+
+  // Input params properties
+  packageXmlFile
+  inputFolder
+  outputFolder
+
+  // Internal properties
+  packageXmlMetadatasTypeLs = []
+  
+
+  async run() {
+    const {args, flags} = this.parse(ExecuteFilter)
+
+    // Get input arguments or default values
+    this.packageXmlFile = flags.packagexml 
+    this.inputFolder = flags.inputfolder || '.'
+    this.outputFolder = flags.outputfolder || 'filteredMetadatas'
+    this.log(`Initialize filtering of ${this.inputFolder} ,using ${this.packageXmlFile} , into ${this.outputFolder}`)
+
+    // Read package.xml file
+    var fs = require('fs'),
+    xml2js = require('xml2js'),
+    util = require('util');
+    var parser = new xml2js.Parser();
+    var self = this
+    fs.readFile(this.packageXmlFile, function (err, data) {
+      parser.parseString(data, function (err2, result) {
+        console.log(util.inspect(result, false, null))
+
+        // get metadata types in parse result
+        try {self.packageXmlMetadatasTypeLs = result.Package.types }
+        catch { throw 'Unable to parse packageXml file '+self.packageXmlFile}
+
+        // Create output folder/empty it if existing
+        if (fs.existsSync(self.outputFolder)) {
+          var emptyDir = require('empty-dir');
+          emptyDir.sync(self.outputFolder);
+        }
+        else {
+          fs.mkdirSync(self.outputFolder)
+        }
+
+        // Process source folder filtering and copy files into target folder
+        self.filterMetadatasByType()
+
+      });
+    });
+
+  }
+
+  // Filter metadatas by type
+  filterMetadatasByType() {
+    var fs = require('fs')
+    var path = require('path')
+    var self = this
+    this.packageXmlMetadatasTypeLs.forEach(function(metadataDefinition){
+      var metadataType = metadataDefinition.name
+      var members = metadataDefinition.members
+
+      // Get metadata description
+      var metadataDesc = self.getMetadataTypeDescription(metadataType)
+      if (metadataDesc == null) {
+         console.log(`- Skipped processing of ${metadataType} : Metadatype Description not found / implemented yet`)
+         return
+      }
+
+      // Browse folder for matching files and copy them into target folder
+      var typeInputFolder = self.inputFolder+'/'+metadataDesc.folder
+      console.log(`- Initialize processing of ${metadataType} , checking folder ${typeInputFolder}`)
+
+      if (fs.existsSync(typeInputFolder)) {
+        console.log(`--- folder ${typeInputFolder} exists !`)
+        var typeOutputFolder = self.outputFolder+'/'+metadataDesc.folder
+        // Create member folder in output folder
+        fs.mkdirSync(typeOutputFolder)
+
+        // Iterate all metadata types members
+        members.forEach(function(member){
+          // Iterate all possible extensions
+          metadataDesc.extensions.forEach(function(ext) {
+            // If input file exists, copy it in output folder
+            var sourceFile = typeInputFolder+'/'+member+ext
+            console.log(`checking ${sourceFile}`)
+            fs.exists(sourceFile, function(exists) { 
+              if (exists) { 
+                var copyTargetFile = typeOutputFolder+'/'+member+ext
+                console.log(`Found ${sourceFile}`)
+                fs.writeFileSync(copyTargetFile, fs.readFileSync(sourceFile))
+              } 
+            }); 
+
+          })
+        })
+      }
+
+    });
+  }
+
+  // get Metadatype description
+  getMetadataTypeDescription (md_type) {
+    var desc = this.describeMetadataTypes()[md_type]
+    return desc
+  }
+
+  // Describe packageXml <=> metadata folder correspondance
+  describeMetadataTypes () {
+    const metadataTypesDescription = {
+      'ApexClass' : { folder: 'classes' , extensions: ['.cls','.cls-meta.xml']}
+    }
+    return metadataTypesDescription
+  }
+}
